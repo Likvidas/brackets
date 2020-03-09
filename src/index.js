df2dc5c918147f7b062d86a9609787f8faa0af7c
Likvidas <bobento@yandex.ru>
2020-03-09 23:52:10 +0300

    init: start Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+
+    const strToArr = str.split('');
+
+    for (let i = 0; i < strToArr.length; i++) {
+        for (let k = 0; k < bracketsConfig.length; k++) {
+            if (strToArr[i] === bracketsConfig[k][0] && strToArr[i + 1] === bracketsConfig[k][1]) {
+                strToArr.splice(i, 2);
+                i = -1;
+            } else {
+                continue;
+            }
+        }
+    }
+
+    return strToArr.length === 0 ? true : false
 }
